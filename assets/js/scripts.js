f105f70d315b9362c38fe546f802cb0f90e3390c
Jhun <jhunreycastillo@gmail.com>
2018-03-06 18:20:26 +0800

    css  and js update
    update css and js style

---
+++ assets/js/scripts.js
@@ -0,0 +1,90 @@
+$(document).on('ready', function() {
+   $(".lender-slide").slick({
+    dots: false,
+    infinite: true,
+ 	speed: 300,
+    centerMode: false,
+    slidesToShow: 8,
+    slidesToScroll: 2,
+    variableWidth: true,
+    responsive: [
+	    {
+	      breakpoint: 1024,
+	      settings: {
+	        slidesToShow: 5,
+	        slidesToScroll: 2,
+	        infinite: true,
+	        dots: true,
+	        variableWidth: false
+	      }
+	    },
+	    {
+	      breakpoint: 768,
+	      settings: {
+	        slidesToShow: 4,
+	        slidesToScroll: 2,
+	        variableWidth: false
+	      }
+	    },
+	    {
+	      breakpoint: 480,
+	      settings: {
+	        slidesToShow: 2,
+	        slidesToScroll: 2,
+	        variableWidth: false
+	      }
+	    }
+  ]
+  });
+
+  $(".page-down").on('click', function(event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+        window.location.hash = hash;
+      });
+    } 
+  });
+});
+
+$('.carousel').carousel({
+  interval: 3000
+})
+
+var theToggle = document.getElementById('toggle');
+function hasClass(elem, className) {
+	return new RegExp(' ' + className + ' ').test(' ' + elem.className + ' ');
+}
+function addClass(elem, className) {
+    if (!hasClass(elem, className)) {
+    	elem.className += ' ' + className;
+    }
+}
+function removeClass(elem, className) {
+	var newClass = ' ' + elem.className.replace( /[\t\r\n]/g, ' ') + ' ';
+	if (hasClass(elem, className)) {
+        while (newClass.indexOf(' ' + className + ' ') >= 0 ) {
+            newClass = newClass.replace(' ' + className + ' ', ' ');
+        }
+        elem.className = newClass.replace(/^\s+|\s+$/g, '');
+    }
+}
+function toggleClass(elem, className) {
+	var newClass = ' ' + elem.className.replace( /[\t\r\n]/g, " " ) + ' ';
+    if (hasClass(elem, className)) {
+        while (newClass.indexOf(" " + className + " ") >= 0 ) {
+            newClass = newClass.replace( " " + className + " " , " " );
+        }
+        elem.className = newClass.replace(/^\s+|\s+$/g, '');
+    } else {
+        elem.className += ' ' + className;
+    }
+}
+
+theToggle.onclick = function() {
+   toggleClass(this, 'on');
+   return false;
+}

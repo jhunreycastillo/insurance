ea5e99245218a4b5efbda7fb662afaac74463638
Jhun <jhunreycastillo@gmail.com>
2018-03-20 14:01:53 +0800

    lic scoreboard
    create lic scoreboard page

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -80,6 +80,7 @@ $('.carousel').carousel({
   interval: 3000
 })
 
+// select script
 var select = '<option>Your Current Age </option>';
    for (i=18;i<=75;i++){
 
@@ -91,6 +92,72 @@ var select = '<option>Your Current Age </option>';
    }
    $('.quote-select-code').html(select);
 
+
+// list grid view script
+$('.click-today').on('click',function(e) {
+
+   if ($(this).hasClass('score-list-box')) {
+       $('.score-today .score-listgrid').removeClass('score-grid-box').addClass('score-list-box');
+   }
+   else if($(this).hasClass('score-grid-box')) {
+       $('.score-today .score-listgrid').removeClass('score-list-box').addClass('score-grid-box');
+   }
+});
+$('.click-last-day').on('click',function(e) {
+   if ($(this).hasClass('score-list-box')) {
+       $('.score-last-day .score-listgrid').removeClass('score-grid-box').addClass('score-list-box');
+   }
+   else if($(this).hasClass('score-grid-box')) {
+       $('.score-last-day .score-listgrid').removeClass('score-list-box').addClass('score-grid-box');
+   }
+});
+
+$('.click-today.score-grid-box').on('click',function(e) {
+    $('.click-today.score-grid-box').addClass('active');
+    $('.click-today.score-list-box').removeClass('active');
+});
+$('.click-today.score-list-box').on('click',function(e) {
+    $('.click-today.score-list-box').addClass('active');
+    $('.click-today.score-grid-box').removeClass('active');
+});
+
+$('.click-last-day.score-grid-box').on('click',function(e) {
+    $('.click-last-day.score-grid-box').addClass('active');
+    $('.click-last-day.score-list-box').removeClass('active');
+});
+$('.click-last-day.score-list-box').on('click',function(e) {
+    $('.click-last-day.score-list-box').addClass('active');
+    $('.click-last-day.score-grid-box').removeClass('active');
+});
+
+
+$(function() {
+    $(".percircle").each(function() {
+        var a = '<div class="slice"><div class="bar"></div><div class="fill"></div></div>',
+            b = $(this).data("percent"),
+            c = 3.6 * b;
+        $(this).find("span").text(b + "%"), $(this).append(a), b >= 50 ? ($(this).find(".slice").css("clip", "rect(auto,auto,auto,auto)"), $(this).find(".bar").css("transform", "rotate(" + c + "deg)"), $(this).addClass("gt50")) : ($(this).find(".slice").css("clip", "rect(0,1em,1em,.5em)"), $(this).find(".bar").css("transform", "rotate(" + c + "deg)"))
+    })
+});
+
+
+$('.score-calendar').supercal({
+  todayButton: true,
+  showInput: false,
+  weekStart: 1,
+  widget: true,
+  cellRatio: 1,
+  format: 'd/m/y',
+  footer: false,
+  dayHeader: true,
+  mode: 'tiny',
+  animDuration: 200,
+  transition: '',
+  tableClasses: 'table table-condensed',
+  hidden: true,
+  setOnMonthChange: true,
+  condensed: false
+});
 
 // mobile menu toggle
 var theToggle = document.getElementById('toggle');

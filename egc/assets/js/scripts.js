06694e3e79a0d41bca1b58cf0436124b4481e03b
Jhun <jhunreycastillo@gmail.com>
2018-03-29 17:04:05 +0800

    egc archive single page
    create egc archive single page

---
+++ egc/assets/js/scripts.js
@@ -0,0 +1,51 @@
+
+$(function() {
+       $(".dropdown").hover(
+           function(){
+             $(this).addClass('open-dropdown'),
+             $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(500);
+           },
+           function(){
+             $(this).removeClass('open-dropdown'),
+             $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
+           }
+       );
+   });
+
+
+
+// mobile menu toggle
+var theToggle = document.getElementById('toggle');
+function hasClass(elem, className) {
+	return new RegExp(' ' + className + ' ').test(' ' + elem.className + ' ');
+}
+function addClass(elem, className) {
+    if (!hasClass(elem, className)) {
+    	elem.className += ' ' + className;
+    }
+}
+function removeClass(elem, className) {
+	var newClass = ' ' + elem.className.replace( /[\t\r\n]/g, ' ') + ' ';
+	if (hasClass(elem, className)) {
+        while (newClass.indexOf(' ' + className + ' ') >= 0 ) {
+            newClass = newClass.replace(' ' + className + ' ', ' ');
+        }
+        elem.className = newClass.replace(/^\s+|\s+$/g, '');
+    }
+}
+function toggleClass(elem, className) {
+	var newClass = ' ' + elem.className.replace( /[\t\r\n]/g, " " ) + ' ';
+    if (hasClass(elem, className)) {
+        while (newClass.indexOf(" " + className + " ") >= 0 ) {
+            newClass = newClass.replace( " " + className + " " , " " );
+        }
+        elem.className = newClass.replace(/^\s+|\s+$/g, '');
+    } else {
+        elem.className += ' ' + className;
+    }
+}
+
+theToggle.onclick = function() {
+   toggleClass(this, 'on');
+   return false;
+}
